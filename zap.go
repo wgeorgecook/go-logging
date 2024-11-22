adf175823754121fd0e220a88b2575a634097339
William Cook <web@williamcook.dev>
2024-11-21 22:30:43 -0500

    Use development zap logger if level is debug

--- zap.go
+++ zap.go
@@ -3,6 +3,15 @@ package gologging
 import "go.uber.org/zap"
 
 func (c *config) start() error {
+	if c.level == DEBUG {
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			return err
+		}
+		c.logger = logger
+		return nil
+	}
+
 	logger, err := zap.NewProduction()
 	if err != nil {
 		return err
